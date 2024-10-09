14a36a041662684dec24b7f626431435d84665ef
logank <logan.aker1989@gmail.com>
2024-10-09 19:38:09 -0400

    Generate qr codes to qr/location/*.png

--- generate_shelves.py
+++ generate_shelves.py
@@ -1,18 +1,19 @@
 import qrcode
 
-WALLS = ['N']
-RACKS = range(1,15)
-SLOTS = range(1,64)
+WALLS = ["N"]
+RACKS = range(1, 2)
+SLOTS = range(1, 65)  # exclusive
 delimiter = "-"
 
 
 def make_qr_code(qr_data):
     qr_code = qrcode.make(qr_data)
     type(qr_code)
-    qr_code.save(f"./generated_codes/{qr_data}.png")
+    qr_code.save(f"./qr/location/{qr_data}.png")
+
 
 for wall in WALLS:
     for rack in RACKS:
         for slot in SLOTS:
-            qr_data = (f"{wall}{delimiter}{rack}{delimiter}{slot}")
+            qr_data = f"{wall}{delimiter}{rack}{delimiter}{slot}"
             make_qr_code(qr_data)
