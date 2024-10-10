d82ae201cc239e6e365bd5a5b4b3e2631c53ecc3
logank <logan.aker1989@gmail.com>
2024-10-09 20:50:01 -0400

    Make main() function to aide in calling from elsewhere.

--- generate_shelves.py
+++ generate_shelves.py
@@ -4,6 +4,16 @@ WALLS = ["N"]
 RACKS = range(1, 2)
 SLOTS = range(1, 65)  # exclusive
 delimiter = "-"
+wrs = []
+
+
+def get_shelf_information():
+    walls = input("Wall Letter: ")
+    racks = range(1, int(input("Racks to create: ")) + 1)
+    slots = range(1, int(input("Slots in rack: ")) + 1)
+    wrs.append(walls)
+    wrs.append(racks)
+    wrs.append(slots)
 
 
 def make_qr_code(qr_data):
@@ -13,8 +23,9 @@ def make_qr_code(qr_data):
 
 
 def main():
-    for wall in WALLS:
-        for rack in RACKS:
-            for slot in SLOTS:
+    get_shelf_information()
+    for wall in wrs[0]:
+        for rack in wrs[1]:
+            for slot in wrs[2]:
                 qr_data = f"{wall}{delimiter}{rack}{delimiter}{slot}"
                 make_qr_code(qr_data)
