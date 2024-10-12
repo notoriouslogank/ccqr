3bb246a2c49fbc06635bdf68d117f530f55c54bf
logank <logan.aker1989@gmail.com>
2024-10-12 17:45:56 -0400

    created functionality (so far) to delete entry.

---
+++ edit_machine.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+import constants
+
+db = mysql.connector.connect(
+    host=constants.HOST,
+    user=constants.USER,
+    passwd=constants.PASSWD,
+    database=constants.DATABASE,
+)
+
+mycursor = db.cursor()
+
+
+def remove_machine_by_client():
+    client = input("Client name to delete: ")
+    sql = "DELETE FROM MachineIDs WHERE client_name = %s"
+    mycursor.execute(sql, (client,))
+    db.commit()
+
+
+remove_machine_by_client()
